5b40522c49c6e9989542b8b6164b3aaca23cbca5
saintric <40336642+saintric@users.noreply.github.com>
2019-10-06 20:09:09 +0800

    automate urls

---
+++ scripts/Python/launch_urls.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python3
+import webbrowser
+#import os
+
+#url1 = "https://hangouts.google.com/?authuser=1"
+#url2 = "https://www.messenger.com/login.php?"
+#url3 = "https://twitter.com/home"
+#url = "https://www.google.com/"
+#
+#webbrowser.open(url1)
+#webbrowser.open(url2)
+#webbrowser.open(url3)
+#webbrowser.open(url)
+
+#urls = "test.urls.txt"
+#for url in urls:
+#    print (url)
+
+file = open('test_urls.txt', 'r', encoding="ANSI") #lists of all url
+urls = file.readlines()
+for url in urls:
+#    print (url)
+    webbrowser.open(url)
+
+#def startupurls(url):
+#    webbrowser.open(url)
+#
+#
+#if __name__ == "__main__":
+#    p = Pool(processes=5)
+#    result = p.map(checkurl, urls)
+#
+#print("done in : ", time.time()-start)
